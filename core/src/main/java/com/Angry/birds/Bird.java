96064f2b23d6171bb406255d9bfbada7468c36c3
vermaarnie <arnieverma2004@gmail.com>
2024-10-25 12:32:07 +0530

    commited...

---
+++ core/src/main/java/com/Angry/birds/Bird.java
@@ -0,0 +1,29 @@
+package com.Angry.birds;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Bird {
+    private Texture texture;
+    private Vector2 position ;
+    public Bird(Texture texture ,float x ,float y){
+        this.texture=texture;
+        this.position=new Vector2(x,y);
+    }
+    public void render(SpriteBatch batch){
+        batch.draw(texture,position.x,position.y);
+    }
+    public void setPosition(float x,float y){
+        position.set(x,y);
+    }
+    public int getx(){
+        return (int) this.position.x;
+    }
+    public int gety(){
+        return (int) this.position.y;
+    }
+    public void dispose(){
+        texture.dispose();
+    }
+}
--- core/src/main/java/com/Angry/birds/Level1Screen.java
+++ core/src/main/java/com/Angry/birds/Level1Screen.java
@@ -16,6 +16,18 @@ public class Level1Screen implements Screen {
 
     private Texture level_background;
     private Texture ground;
+    private Bird bird1;
+    private Bird bird2;
+    private Bird bird3;
+    private Block block1;
+    private Block block2;
+    private Block block3;
+    private  Block block4;
+    private  Block block5;
+    private  Block block6;
+    private Pig pig1;
+    private Pig pig2;
+    private Pig pig3;
 
     private Stage stg;
     private Game game;
@@ -35,12 +47,25 @@ public class Level1Screen implements Screen {
         catp_2 = new Texture(Gdx.files.internal("catp2.png"));
 
 
+
+
         level_background = new Texture(Gdx.files.internal("level_background.png"));
         ground = new Texture(Gdx.files.internal("ground.png"));
         stg = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stg);
         skin = new Skin(Gdx.files.internal("uiskin.json"));
-
+        bird1=new Bird(new Texture(Gdx.files.internal("Red.png")),10,80);
+        bird2=new Bird(new Texture(Gdx.files.internal("Red.png")),50,80);
+        bird3=new Bird(new Texture(Gdx.files.internal("Red.png")),110,150);
+        pig1 = new Pig(new Texture(Gdx.files.internal("pig.png")),540,143);
+        pig2 = new Pig(new Texture(Gdx.files.internal("pig.png")),610,213);
+        pig3 = new Pig(new Texture(Gdx.files.internal("pig.png")),680,143);
+        block1 = new Block(new Texture(Gdx.files.internal("Wood1.png")),540,78);
+        block2 = new Block(new Texture(Gdx.files.internal("Wood1.png")),610,78);
+        block3 = new Block(new Texture(Gdx.files.internal("Wood1.png")),680,78);
+        block4 = new Block(new Texture(Gdx.files.internal("Wood2.png")),470,78);
+        block5 = new Block(new Texture(Gdx.files.internal("Wood1.png")),610,147);
+        block6 = new Block(new Texture(Gdx.files.internal("Wood2_Rotated.png")),743,78);
         TextButton pause_button = new TextButton("Pause",skin);
         pause_button.setPosition(10,Gdx.graphics.getHeight() - 50);
         pause_button.addListener(new ClickListener(){
@@ -67,7 +92,22 @@ public class Level1Screen implements Screen {
         batch.draw(level_background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
         batch.draw(ground,0,0,Gdx.graphics.getWidth(),100);
         batch.draw(catp_1,100,80);
+        bird3.render(batch);
         batch.draw(catp_2,100,120);
+        bird1.render(batch);
+        bird2.render(batch);
+
+        block1.render(batch);
+        block2.render(batch);
+        block3.render(batch);
+        block4.render(batch);
+        block5.render(batch);
+        block6.render(batch);
+        pig1.render(batch);
+        pig2.render(batch);
+        pig3.render(batch);
+
+
         batch.end();
 
         stg.act(delta);
@@ -102,6 +142,19 @@ public class Level1Screen implements Screen {
     public void dispose() {
         catp_1.dispose();
         catp_2.dispose();
+        bird1.dispose();
+        bird2.dispose();
+        bird3.dispose();
+        pig1.dispose();
+        pig2.dispose();
+        pig3.dispose();
+        block1.dispose();
+        block2.dispose();
+        block3.dispose();
+        block4.dispose();
+        block5.dispose();
+        block6.dispose();
+
 
         batch.dispose();
         level_background.dispose();
